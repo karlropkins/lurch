61a7dd6d9aebb568b80cd7f0705674135df0ca5c
Karl Ropkins <k.ropkins@its.leeds.ac.uk>
2025-07-25 16:38:14 +0100

    folders
    folders

---
+++ R/lurch-package.R
@@ -0,0 +1,16 @@
+##################################################
+#' lurch: A bare-bones R package
+##################################################
+#'
+#' A bare-bones R Package build used to test code, functions, data,
+#' imports, etc... Can be a bit of a messy build at times...
+#'
+#' This package was splat packed.
+#'
+#' @section package functions:
+#' [to be added]
+#'
+#' @name lurch-package
+#' @aliases lurch
+#' "_PACKAGE"
+NULL
